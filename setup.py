33ef8d9864645c112c9752f8bc65052adc669128
Kenny Do <chinesedewey@gmail.com>
2016-09-04 13:31:13 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pick-my-stick',
+    version='0.0.1',
+    description='Stickers for Slack',
+    long_description='S t i c k e r s',
+    url='https://github.com/kennydo/pick-my-stick',
+    author='Kenny Do',
+    author_email='chinesedewey@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet',
+    ],
+    packages=find_packages(),
+    entry_points={
+    },
+)
